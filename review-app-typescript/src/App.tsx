9b75aa3718d555cf612e7c2ed03f5ee0d094d64e
Ronnie <43523873+BlissfulCoda@users.noreply.github.com>
2022-09-21 21:16:56 +0100

    feat(client): create feedbacklist and feedbackItem component

--- review-app-typescript/src/App.tsx
+++ review-app-typescript/src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import FeedbackData, { FeedbackDataInterface } from "./Data/FeedbackData";
+
 import Header from "./Components/Header";
+import FeedbackList from "./Components/FeedbackList";
 
 export default function App() {
+  const [feedback, setFeedback] =
+    useState<FeedbackDataInterface[]>(FeedbackData);
   return (
     <div>
       <Header title="Review App" />
+      <div className="container">
+        <FeedbackList feedback={feedback} />
+      </div>
     </div>
   );
 }
