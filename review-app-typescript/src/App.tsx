f8e3e89cf0ef7ea0e10bca98113e5c9119b82dc5
Ronnie <43523873+BlissfulCoda@users.noreply.github.com>
2022-09-21 22:41:30 +0100

    feat(client): add feedbackStats

--- review-app-typescript/src/App.tsx
+++ review-app-typescript/src/App.tsx
@@ -3,6 +3,7 @@ import FeedbackData, { FeedbackDataInterface } from "./Data/FeedbackData";
 
 import Header from "./Components/Header";
 import FeedbackList from "./Components/FeedbackList";
+import FeedbackStats from "./Components/FeedbackStats";
 
 export default function App() {
   const [feedback, setFeedback] =
@@ -11,6 +12,7 @@ export default function App() {
     <div>
       <Header title="Review App" />
       <div className="container">
+        <FeedbackStats feedback={feedback}/>
         <FeedbackList feedback={feedback} />
       </div>
     </div>
